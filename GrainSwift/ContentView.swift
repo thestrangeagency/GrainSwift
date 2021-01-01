909d7c9a5cd0966b6ce9a54110af04c6491276f6
lucas <lucas@thestrangeagency.com>
2021-01-01 14:17:40 +0100

    initial UI and file loading ok

--- GrainSwift/ContentView.swift
+++ GrainSwift/ContentView.swift
@@ -6,11 +6,18 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, O World, O Earth!")
             .padding()
+            .onAppear {
+                if let audioFileUrl = Bundle.main.url(forResource: "test", withExtension: "wav") {
+                    let audioFile:AVAudioFile? = try! AVAudioFile(forReading: audioFileUrl)
+                    print("opened a file with sample rate: \(audioFile?.fileFormat.sampleRate ?? 0.0)")
+                }
+            }
     }
 }
 
