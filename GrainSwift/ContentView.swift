8dba0b96b27a9a31ed77ce2a9ffa6ab5968c36c0
lucas <lucas@thestrangeagency.com>
2021-01-01 13:29:27 +0100

    Initial Commit

---
+++ GrainSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GrainSwift
+//
+//  Created by lucas kuzma on 1/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GrainSwift/GrainSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrainSwiftApp.swift
+//  GrainSwift
+//
+//  Created by lucas kuzma on 1/1/21.
+//
+
+import SwiftUI
+
+@main
+struct GrainSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
