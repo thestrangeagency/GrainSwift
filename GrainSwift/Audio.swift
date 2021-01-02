c0de958f0f6aea673e866dd756124dd1186d0e0f
lucas <lucas@thestrangeagency.com>
2021-01-02 14:10:11 +0100

    set up audio engine and play audio

--- GrainSwift/Audio.swift
+++ GrainSwift/Audio.swift
@@ -6,7 +6,50 @@
 //
 
 import Foundation
+import AVFoundation
 
 class Audio: ObservableObject {
     @Published var source = AudioSource()
+    
+    let engine = AVAudioEngine()
+    var bufferIndex:AVAudioFrameCount = 0;
+    
+    init() {
+        let mainMixer = engine.mainMixerNode
+        let output = engine.outputNode
+        let outputFormat = output.inputFormat(forBus: 0)
+        let inputFormat = source?.audioFile.processingFormat
+   
+        let sourceNode = AVAudioSourceNode { _, _, frameCount, audioBufferList -> OSStatus in
+            
+            guard let audioBuffer = self.source?.audioBuffer,
+                  let channelData = audioBuffer.floatChannelData else {
+                return noErr
+            }
+            
+            let bufferListPointer = UnsafeMutableAudioBufferListPointer(audioBufferList)
+        
+            for frame in 0..<Int(frameCount) {
+                
+                for channel in 0..<bufferListPointer.count {
+                    let outBuffer:UnsafeMutableBufferPointer<Float> = UnsafeMutableBufferPointer(bufferListPointer[channel])
+                    outBuffer[frame] = channelData[channel][Int(self.bufferIndex)]
+                }
+                
+                self.bufferIndex = (self.bufferIndex + 1) % audioBuffer.frameLength
+            }
+            return noErr
+        }
+        
+        engine.attach(sourceNode)
+        engine.connect(sourceNode, to: mainMixer, format: inputFormat)
+        engine.connect(mainMixer, to: output, format: outputFormat)
+        
+        do {
+            try engine.start()
+            print("engine started")
+        } catch {
+            print("could not start engine: \(error)")
+        }
+    }
 }
