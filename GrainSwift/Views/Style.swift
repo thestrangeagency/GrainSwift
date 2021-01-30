228309c2bd07d6a307d07cfdb112e73b0915da0c
lucas <lucas@thestrangeagency.com>
2021-01-30 11:43:50 +0100

    tweak color math

--- GrainSwift/Views/Style.swift
+++ GrainSwift/Views/Style.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct Style {
     static func colorFor(x: Double, y: Double) -> Color {
-        return Color(red: 1.0 - x, green: y, blue: 1.0, opacity: 0.5)
+        return Color(red: 1.0 - y, green: 1.0 - x, blue: 1.0, opacity: 0.5)
     }
     
     static var foreground: Color {
@@ -21,7 +21,7 @@ struct Style {
     
     static var background: Color {
         get {
-            return Self.colorFor(x: 1, y: 0)
+            return Self.colorFor(x: 1, y: 1)
         }
     }
 }
